bc4e0551aaa3ff74882440417c11fd631c6a002c
thai <thai@example.com>
2025-10-08 00:12:35 +0700

    Quản lý danh mục sản phẩm P1

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,12 +2,18 @@ const systemConfig = require("../../config/system");
 
 const dashboardRoutes = require("./dashboard.route");
 
-const productRoutes = require("./product.route")
+const productRoutes = require("./product.route");
+
+const productCategoryRoutes = require("./product-category.route")
+
 
 module.exports = (app) => {
     PathAdmin = systemConfig.prefixAdmin;
     app.use(PathAdmin + '/dashboard', dashboardRoutes);
 
     app.use(PathAdmin + '/products', productRoutes);
+
+    app.use(PathAdmin + '/products-category', productCategoryRoutes);
+
 
 }
