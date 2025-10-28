d714b4f9c73e62b2ad214c733a32a3a54f00cb50
thai <thai@example.com>
2025-10-28 18:40:36 +0700

    Tạo collection accounts

---
+++ models/account.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const generate = require("../helper/generate")
+
+const accountSchema = new mongoose.Schema(
+    {
+        fullName: String,
+        email: String,
+        password: String,
+        token: {
+            type: String,
+            default: generate.generateRandomString(20)
+        },
+        phone: String,
+        avatar: String,
+        role_id: String,
+        status: String,
+        deletedAt: Date,
+        deleted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Account = mongoose.model('Product', productSchema, "products");
+
+module.exports = Account;
