959561a7824061678540114fdeb5a4ddcc3fb5f6
thai <thai@example.com>
2025-10-31 19:52:30 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -7,17 +7,25 @@ module.exports.index = async (req, res) => {
 
     // Lây ra sản phẩm nổi bật
     const productsFeatured = await Product.find({
-        featured: 1,
+        featured: "1",
         deleted: false,
         status: "active"
-    }).limit(3);
+    }).limit(6);
 
-    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
     // Hết lấy ra sản phẩm nổi bật
 
-    console.log(newProducts);
+    //Lấy ra sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"  
+    }).sort({position:"desc"}).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+    // Hết lấy ra sản phẩm mới nhất
     res.render("clients/pages/home/index", {
         pageTitle: "Trang chu",
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     })
 }
