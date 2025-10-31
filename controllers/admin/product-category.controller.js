3828559801f2018df1e5be9f100f1052bdc7ff38
thai <thai@example.com>
2025-10-31 18:18:45 +0700

    Fix phân quyền bên backend

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -39,23 +39,30 @@ module.exports.create = async (req, res) => {
 
 //[POST] / admin/products-category/create
 module.exports.createPost = async (req, res) => {
+    
+    const permissions = res.locals.role.permissions;
+    if (permissions.include("product-category_create")) {
+        let find = {
+            deleted: false,
+        };
+        console.log(req.body);
+        if (req.body.position == "") {
+            const countProducts = await ProductCategory.countDocuments();
+            req.body.position = countProducts + 1;
+        }
+        else {
+            req.body.position = parseInt(req.body.position);
+        }
 
-    let find = {
-        deleted: false,
-    };
-    console.log(req.body);
-    if (req.body.position == "") {
-        const countProducts = await ProductCategory.countDocuments();
-        req.body.position = countProducts + 1;
+        const record = new ProductCategory(req.body);
+        await record.save();
+
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+    } else {
+        return;
     }
-    else {
-        req.body.position = parseInt(req.body.position);
-    }
 
-    const record = new ProductCategory(req.body);
-    await record.save();
 
-    res.redirect(`${systemConfig.prefixAdmin}/products-category`)
 }
 
 //[GET] / admin/products-category/edit/:id
