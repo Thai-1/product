5c93024263fe9407b1722d0540576a0463b248e9
thai <thai@example.com>
2025-11-09 22:13:19 +0700

    Tối ưu lại folder socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,51 +1,15 @@
 const Chat = require("../../models/chat.moddule")
 const User = require("../../models/user.model")
 
-const uploadToCloudinary = require("../../helper/uploadToCloudinary")
-
+const chatSocket = require("../../sockets/client/chat.socket")
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-    const userId = res.locals.user.id;
-    const fullName = res.locals.user.fullName;
-
+    
     // Socket IO
-    _io.once('connection', (socket) => {
-        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-            let images = [];
-
-            for (const imageBuffer of data.images) {
-                const link = await uploadToCloudinary(imageBuffer);
-                images.push(link);
-            }
-
-            //Luu vao DB
-            const chat = new Chat({
-                user_id: userId,
-                content: data.content,
-                images: images
-            })
-            await chat.save();
-
-            // Trả data về client
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                fullName: fullName,
-                userId: userId,
-                content: data.content,
-                images: images
-            })
-        })
-        // Typing
-        socket.on("CLIENT_SEND_TYPING", async (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
-                fullName: fullName,
-                userId: userId,
-                type: type
-            })
-        })
-        // End Typing
-    })
+    chatSocket(res);
     // End Socket IO
+
 
     // Lấy data từ DB
     const chats = await Chat.find({
---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,44 @@
+const Chat = require("../../models/chat.moddule")
+
+const uploadToCloudinary = require("../../helper/uploadToCloudinary")
+
+
+module.exports = () => {
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+            let images = [];
+
+            for (const imageBuffer of data.images) {
+                const link = await uploadToCloudinary(imageBuffer);
+                images.push(link);
+            }
+
+            //Luu vao DB
+            const chat = new Chat({
+                user_id: userId,
+                content: data.content,
+                images: images
+            })
+            await chat.save();
+
+            // Trả data về client
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                fullName: fullName,
+                userId: userId,
+                content: data.content,
+                images: images
+            })
+        })
+        // Typing
+        socket.on("CLIENT_SEND_TYPING", async (type) => {
+            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+                fullName: fullName,
+                userId: userId,
+                type: type
+            })
+        })
+        // End Typing
+    })
+}
