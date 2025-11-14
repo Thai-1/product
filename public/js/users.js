29c11fadd948173dfe965ab56a524ea3a6f47b5a
thai <thai@example.com>
2025-11-14 20:09:36 +0700

    Khi A gửi kết bạn cho B danh sách người dùng của B xóa đi A

--- public/js/users.js
+++ public/js/users.js
@@ -81,46 +81,60 @@ if (badgeUsersAccept) {
 //End SERVER RETURN LENGTH ACCEPT FRIEND
 
 // SERVER_RETURN_INFO_ACCEPT_FRIEND
-const dataUsersAccept = document.querySelector("[data-users-accept]");
-if (dataUsersAccept) {
-    const userId = dataUsersAccept.getAttribute("data-users-accept");
-    socket.on("SERVER_RETURN_INFO_ACCEPT_FRIEND", (data) => {
+socket.on("SERVER_RETURN_INFO_ACCEPT_FRIEND", (data) => {
+    // Trang lời mời đã nhận
+    const dataUsersAccept = document.querySelector("[data-users-accept]");
+    if (dataUsersAccept) {
+        const userId = dataUsersAccept.getAttribute("data-users-accept");
+    }
+    if (userId == data.userId) {
+        // Vẽ user ra giao diện
+        const div = document.createElement("div");
+        div.classList.add("col-6");
+
+        div.setAttribute("user-id", data.infoUserA._id);
+
+        div.innerHTML = `<div class="box-user"><div class="inner-avatar"><img src="https://robohash.org/hicveldicta.png" alt=${data.infoUserA.fullName}></div><div class="inner-info"><div class="inner-name">${data.infoUserA.fullName}</div><div class="inner-buttons"><button class="btn btn-sm btn-primary mr-1" btn-accept-friend=${data.infoUserA.fullName._id}>Chấp nhận</button><button class="btn btn-sm btn-secondary mr-1" btn-refuse-friend="690b7dd3ba50861aad2e8211">Xóa </button><button class="btn btn-sm btn-secondary mr-1" btn-deleted-friend="690b7dd3ba50861aad2e8211" disabled="">Đã xóa </button><button class="btn btn-sm btn-secondary mr-1" btn-accepted-friend="690b7dd3ba50861aad2e8211" disabled="">Đã chấp nhận</button></div></div></div></div>`
+
+        dataUsersAccept.appendChild(div);
+        // Hết vẽ user ra giao diện
+
+        //Hủy lời mời kết bạn
+        const buttonRefuse = div.querySelector("[btn-refuse-friend]");
+
+        refuseFriend(buttonRefuse);
+        //Hết hủy lời mời kết bạn
+
+        // Chấp nhận lời mời kết bạn
+        const buttonAccept = div.querySelector("[btn-accept-friend]");
+        acceptFriend(buttonAccept);
+        // Hết Chấp nhận lời mời kết bạn
+    }
+
+    //Trang danh sách người dùng
+    const dataUsersNotFriend = document.querySelector("[data-users-not-friend]");
+    if (dataUsersNotFriend) {
+        const userId = dataUsersNotFriend.getAttribute("data-users-not-friend");
         if (userId == data.userId) {
-            // Vẽ user ra giao diện
-            const div = document.createElement("div");
-            div.classList.add("col-6");
-
-            div.setAttribute("user-id", data.infoUserA._id);
-
-            div.innerHTML = `<div class="box-user"><div class="inner-avatar"><img src="https://robohash.org/hicveldicta.png" alt=${data.infoUserA.fullName}></div><div class="inner-info"><div class="inner-name">${data.infoUserA.fullName}</div><div class="inner-buttons"><button class="btn btn-sm btn-primary mr-1" btn-accept-friend=${data.infoUserA.fullName._id}>Chấp nhận</button><button class="btn btn-sm btn-secondary mr-1" btn-refuse-friend="690b7dd3ba50861aad2e8211">Xóa </button><button class="btn btn-sm btn-secondary mr-1" btn-deleted-friend="690b7dd3ba50861aad2e8211" disabled="">Đã xóa </button><button class="btn btn-sm btn-secondary mr-1" btn-accepted-friend="690b7dd3ba50861aad2e8211" disabled="">Đã chấp nhận</button></div></div></div></div>`
-
-            dataUsersAccept.appendChild(div);
-            // Hết vẽ user ra giao diện
-
-            //Hủy lời mời kết bạn
-            const buttonRefuse = div.querySelector("[btn-refuse-friend]");
-
-            refuseFriend(buttonRefuse);
-            //Hết hủy lời mời kết bạn
-
-            // Chấp nhận lời mời kết bạn
-            const buttonAccept = div.querySelector("[btn-accept-friend]");
-            acceptFriend(buttonAccept);
-            // Hết Chấp nhận lời mời kết bạn
-
+            const boxUserRemove = dataUsersNotFriend.querySelector(`[user-id='${data.infoUserA._id}']`);
+            if (boxUserRemove) {
+                dataUsersNotFriend.removeChild(boxUserRemove);
+            }
         }
-    })
-}
+    }
+
+})
+
 //END SERVER_RETURN_INFO_ACCEPT_FRIEND
 
 
 // SERVER_RETURN_USER_ID_CANCEL_FRIEND
 socket.on("SERVER_RETURN_USER_ID_CANCEL_FRIEND", (data) => {
     const boxUserRemove = document.querySelector(`[user-id='${data.userIdA}']`);
-    if(boxUserRemove) {
+    if (boxUserRemove) {
         const dataUsersAccept = document.querySelector("[data-users-accept]");
         const userIdB = badgeUsersAccept.getAttribute("badge-users-accept");
-        if(userIdB == data.userIdB)
+        if (userIdB == data.userIdB)
             dataUsersAccept.removeChild(boxUserRemove);
     }
 })
