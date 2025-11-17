bcd5ef1d0ec61a338abd57c529c105fbdf5c5ffa
thai <thai@example.com>
2025-11-17 18:44:05 +0700

    Fix hiển thị danh sách người dùng trừ bạn bè

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -16,6 +16,8 @@ module.exports.notFriend = async (req, res) => {
 
     const requestFriends = myUser.requestFriends;
     const acceptFriends = myUser.acceptFriends;
+    const friendList = myUser.friendList;
+    const friendListId = friendList.map(item => item.user_id)
 
     const users = await User.find({
         $and: [
@@ -23,6 +25,8 @@ module.exports.notFriend = async (req, res) => {
             { _id: { $ne: userId } },
             { _id: { $nin: requestFriends } },
             { _id: { $nin: acceptFriends } },
+            { _id: { $nin: friendListId } },
+
         ],
         status: "active",
         deleted: false
